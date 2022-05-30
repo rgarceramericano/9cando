3f11e8d70dac3b1bb8e0b9d912c791dab07e7102
rgarceramericano <49561060+rgarceramericano@users.noreply.github.com>
2022-05-30 08:59:41 -0500

    Add files via upload

---
+++ Python/1er Trimestre/hola.py
@@ -0,0 +1,25 @@
+frutas=["mango","limon"]
+
+print(frutas)
+
+print(frutas[1])
+
+print(len(frutas))
+
+frutas.append("fresa")
+
+print(frutas)
+
+frutas.append("zarzamora")
+
+print(frutas)
+
+frutas.sort()
+
+print(frutas)
+
+porcomprar=input("ingresa una fruta: ")
+
+frutas.append(porcomprar)
+
+print(frutas)
